3ceb574a757379d7b38e3e60c1f415659e5df33a
FUSER93 <37194695+FUSER93@users.noreply.github.com>
2018-05-10 17:18:03 -0300

    PHP-INDEX+FORM

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav class="menu">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="login.html">Iniciar sesión</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ header_index.php
@@ -0,0 +1,9 @@
+
+    <header>
+      <h2><a href="#">Website Logo</a></h2>
+      <nav>
+        <li><a href="formulario2.php">¡Registrate!</a></li>
+        <li> | </li>
+        <li><a href="login.html">Login</a></li>
+      </nav>
+    </header>
