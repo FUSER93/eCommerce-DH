f5d19e35adc2d166cd235054d4e383ad30ff4942
Sofía Nuñez <31999837+sofiaanunez@users.noreply.github.com>
2018-05-17 22:06:48 -0300

    Update header_index.php

--- header_index.php
+++ header_index.php
@@ -4,6 +4,6 @@
       <nav>
         <li><a href="formulario2.php">¡Registrate!</a></li>
         <li> | </li>
-        <li><a href="login.html">Login</a></li>
+        <li><a href="login.php">Login</a></li>
       </nav>
     </header>
