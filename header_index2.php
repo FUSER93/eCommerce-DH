15ae8d2fa17fb61fa60334118c8da82f68657384
Sofía Nuñez <31999837+sofiaanunez@users.noreply.github.com>
2018-05-17 22:07:17 -0300

    Update header_index2.php

--- header_index2.php
+++ header_index2.php
@@ -3,9 +3,9 @@
       <h2><a href="#">Website Logo</a></h2>
       <nav>
         <?php if (no estoy logueado) {?>
-        <li><a href="formulario2.html">¡Registrate!</a></li>
+        <li><a href="formulario2.php">¡Registrate!</a></li>
         <li> | </li>
-        <li><a href="login.html">Login</a></li>
+        <li><a href="login.php">Login</a></li>
       <?php else{?>
         <li>Mi Perfil</li>
         <li>Logout</li>
